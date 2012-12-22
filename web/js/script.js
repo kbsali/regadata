682816d44feb0155ba1eb8030012934f7c98b456
Kevin Saliou <kevin@saliou.name>
2012-12-22 16:51:29 +0100

    mark report's max values in bold

--- web/js/script.js
+++ web/js/script.js
@@ -1,7 +1,22 @@
+function getMax(_class) {
+    var max = 0, tmp, idx;
+    $("#report td."+_class).each(function(rowIndex) {
+        tmp = parseFloat($(this).text());
+        if(max < tmp) {
+            max = tmp;
+            $sel = $(this);
+        }
+    });
+    $sel.css("font-weight", "bold");
+}
 !function ($) {
-    $(function(){
-        $('.tooltips').tooltip({
+    $(function() {
+        $(".tooltips").tooltip({
             selector: "a[rel=tooltip]"
-        })
+        });
+        var clmax = ["1hspeed", "1hvmg", "lrspeed", "lrvmg", "lrdistance", "24hspeed", "24hvmg", "24hdistance"];
+        for(var i=0;i<clmax.length;i++) {
+            getMax(clmax[i])
+        }
     })
 }(window.jQuery)
